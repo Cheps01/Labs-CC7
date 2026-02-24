38bdfa571c2dfa52c966e219dbd7fdbe1edb12a7
Cheps01 <josecarlosgordilloa@hotmail.com>
2026-02-23 19:16:50 -0600

    Minor bug fix.

--- Lab2/app/main.c
+++ Lab2/app/main.c
@@ -10,13 +10,13 @@ void main() {
     PRINT(" / /__/ _ `/ _ \\/ __/  /___/ / /__/ _ `/ / __/ // / / _ `/ _  / _ \\/ __/ _ `/ \n");
     PRINT("/____/\\_,_/_.__/____/        \\___/\\_,_/_/\\__/\\_,_/_/\\_,_/\\_,_/\\___/_/  \\_,_/ \n");
     while (1) {
-        // PRINT("--------------------------------------\n");
-        // PRINT("Ingrese primer numero: ");
-        // READ("%d", &num1);
-        // PRINT("Ingrese segundo numero: ");
-        // READ("%d", &num2);
-        // sum = num1 + num2;
-        // PRINT("%d + %d = %d \n", num1, num2, sum);
+        PRINT("--------------------------------------\n");
+        PRINT("Ingrese primer numero: ");
+        READ("%d", &num1);
+        PRINT("Ingrese segundo numero: ");
+        READ("%d", &num2);
+        sum = num1 + num2;
+        PRINT("%d + %d = %d \n", num1, num2, sum);
 
         PRINT("--------------------------------------\n");
         PRINT("Ingrese primer numero decimal: ");
